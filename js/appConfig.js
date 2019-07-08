e13b8fee741ce649afb1e350070ad2ac917a0c58
sysnetcz <info@sysnet.cz>
2019-07-08 12:27:07 +0200

    sekm

--- js/appConfig.js
+++ js/appConfig.js
@@ -10,6 +10,7 @@ const {SearchProviders, searchProviderFactory} = require('./SearchProviders');
 const EditingInterface = require('./EditingInterface');
 const renderHelp = require('./Help');
 
+
 module.exports = {
     initialState: {
         defaultState: {},
@@ -123,3 +124,6 @@ module.exports = {
         }
    }
 };
+
+Proj4js.defs('EPSG:5514', '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +pm=greenwich +units=m +no_defs towgs84=570.8,85.7,462.8,4.998,1.587,5.261,3.56');
+
